46c5f6e5f4cf9162dc1180b51f577144d3915754
Jesper Qvist <jgq@live.dk>
2019-08-26 19:10:01 +0200

    halvvejs gennem kapitel models

--- jobs/models.py
+++ jobs/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your JOBS models here.
 class Job(models.Model):
-    
+    image = models.ImageField(upload_to='')
+    summary = models.CharField(max_length=200)
